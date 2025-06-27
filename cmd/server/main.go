eea9bab50ad24e57e8505f2e0f91bc32ba1cd471
Omar Khodr <omar.khoder2000@gmail.com>
2025-06-27 18:54:30 +0200

    Remove old toy examples of log package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"log"
-
-	"github.com/omarkhodr/proglog/internal/server"
 )
 
 func main() {
-	srv := server.NewHttpServer(":8080")
-	log.Fatal(srv.ListenAndServe())
+	log.Println("TODO: Implement distributed key-value log.")
 }
