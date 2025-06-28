5e4eb45035b36d5a67dc3c68f16b940e90d1b72b
Omar Khodr <omar.khoder2000@gmail.com>
2025-06-28 20:22:16 +0200

    Server-to-Server Service Discovery.

---
+++ internal/agent/agent_test.go
@@ -0,0 +1,145 @@
+package agent_test
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	api "github.com/omarkhodr/proglog/api/v1"
+	"github.com/omarkhodr/proglog/internal/agent"
+	"github.com/omarkhodr/proglog/internal/config"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func TestAgent(t *testing.T) {
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		Server:        true,
+		ServerAddress: "127.0.0.1",
+	})
+	require.NoError(t, err)
+
+	peerTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.RootClientCertFile,
+		KeyFile:       config.RootClientKeyFile,
+		CAFile:        config.CAFile,
+		Server:        false,
+		ServerAddress: "127.0.0.1",
+	})
+	require.NoError(t, err)
+
+	var agents []*agent.Agent
+	for i := range 3 {
+		ports := dynaport.Get(2)
+		bindAddr := fmt.Sprintf("%s:%d", "127.0.0.1", ports[0])
+		rpcPort := ports[1]
+
+		dataDir, err := os.MkdirTemp("", "agent-test-log")
+		require.NoError(t, err)
+
+		var startJoinAddrs []string
+		if i != 0 {
+			startJoinAddrs = append(startJoinAddrs, agents[0].Config.BindAddr)
+		}
+
+		agent, err := agent.New(agent.Config{
+			NodeName:        fmt.Sprintf("%d", i),
+			StartJoinAddrs:  startJoinAddrs,
+			BindAddr:        bindAddr,
+			RPCPort:         rpcPort,
+			DataDir:         dataDir,
+			ACLModelFIle:    config.ACLModelFile,
+			ACLPolicyFile:   config.ACLPolicyFile,
+			ServerTLSConfig: serverTLSConfig,
+			PeerTLSConfig:   peerTLSConfig,
+		})
+		require.NoError(t, err)
+
+		agents = append(agents, agent)
+	}
+	defer func() {
+		for _, agent := range agents {
+			err := agent.Shutdown()
+			require.NoError(t, err)
+			require.NoError(t, os.RemoveAll(agent.Config.DataDir))
+		}
+	}()
+	time.Sleep(3 * time.Second)
+
+	leaderClient := client(t, agents[0], peerTLSConfig)
+	produceResponse, err := leaderClient.Produce(
+		context.Background(),
+		&api.ProduceRequest{
+			Record: &api.Record{
+				Value: []byte("foo"),
+			},
+		},
+	)
+	require.NoError(t, err)
+	consumeResponse, err := leaderClient.Consume(
+		context.Background(),
+		&api.ConsumeRequest{
+			Offset: produceResponse.Offset,
+		},
+	)
+	require.NoError(t, err)
+	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
+
+	// Wait until replication has finished
+	time.Sleep(3 * time.Second)
+
+	followerClient := client(t, agents[1], peerTLSConfig)
+	consumeResponse, err = followerClient.Consume(
+		context.Background(),
+		&api.ConsumeRequest{
+			Offset: produceResponse.Offset,
+		},
+	)
+	require.NoError(t, err)
+	require.Equal(t, consumeResponse.Record.Value, []byte("foo"))
+
+	// Currently, adding this code to the test would make it fail, even though
+	// this is a reasonable assertion to make. There should only be one record
+	// in the leader's log.
+	//
+	// The problem with our implementation is that when one server discovers,
+	// another, they replicate each other in a cycle. We need to define a leader
+	// client and follower clients, so followers only copy from the leader.
+	// We'll implement this using consensus in Raft.
+	//
+	// consumeResponse, err = leaderClient.Consume(
+	// 	context.Background(),
+	// 	&api.ConsumeRequest{
+	// 		Offset: produceResponse.Offset + 1,
+	// 	},
+	// )
+	// require.Nil(t, consumeResponse)
+	// require.Error(t, err)
+	// got := status.Code(err)
+	// want := status.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
+	// require.Equal(t, got, want)
+
+}
+
+func client(
+	t *testing.T,
+	agent *agent.Agent,
+	tlsConfig *tls.Config,
+) api.LogClient {
+	tlsCreds := credentials.NewTLS(tlsConfig)
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(tlsCreds)}
+	rpcAddr, err := agent.Config.RPCAddr()
+	require.NoError(t, err)
+	conn, err := grpc.NewClient(fmt.Sprintf("%s", rpcAddr), opts...)
+	require.NoError(t, err)
+	client := api.NewLogClient(conn)
+	return client
+}
